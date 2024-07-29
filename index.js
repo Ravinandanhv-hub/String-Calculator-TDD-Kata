ef97e58f10fa5c82c1b55af4065e025372bc7696
Ravinandan H V <ravinandan@appveen.com>
2024-07-29 20:30:31 +0530

    TC paased for new line

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ class StringCalculator {
       if (numbers === "") return 0;
 
       if(numbers){
-        const sum = numbers.split(',').reduce((total , num)=>{
+        const sum = numbers.split(/,|\n/)?.reduce((total , num) => {
             return total + Number(num);
         }, 0)
         return sum;
       }
     }
 }
+
+let calculator = new StringCalculator
+calculator.add('1\n2,3')
   
 module.exports = StringCalculator;

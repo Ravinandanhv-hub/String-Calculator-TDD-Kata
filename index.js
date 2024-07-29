2ef4677246a3f2395576fd3af4367bebd0320ee0
Ravinandan H V <ravinandan@appveen.com>
2024-07-29 20:39:14 +0530

    TC paased for Support different delimiters

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ class StringCalculator {
     add(numbers) {
       if (numbers === "") return 0;
 
+      let delimiter = /,|\n/;
+      if (numbers.startsWith("//")) {
+        const delimiterEndIndex = numbers.indexOf("\n");
+        delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+        numbers = numbers.substring(delimiterEndIndex + 1);
+      }
+
       if(numbers){
-        const sum = numbers.split(/,|\n/)?.reduce((total , num) => {
+        const sum = numbers.split(delimiter)?.reduce((total , num) => {
             return total + Number(num);
         }, 0)
         return sum;
@@ -12,6 +19,6 @@ class StringCalculator {
 }
 
 let calculator = new StringCalculator
-calculator.add('1\n2,3')
+calculator.add('//;\n1;2')
   
 module.exports = StringCalculator;

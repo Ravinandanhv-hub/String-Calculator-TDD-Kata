6c3c879f23377e9b22af7c59c6e576943d6ffb8d
Ravinandan H V <ravinandan@appveen.com>
2024-07-29 20:10:48 +0530

    TC and TC pased for '1' and '1,2' string

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 class StringCalculator {
     add(numbers) {
-      if (numbers == ""){
-        return 0;
-      } 
+      if (numbers === "") return 0;
+
+      if(numbers){
+        const numbersArray = numbers.split(',');
+        const sum = numbersArray.reduce((total , num)=>{
+            return total + Number(num);
+        }, 0)
+        return sum;
+      }
     }
 }
   
--- test.js
+++ test.js
@@ -6,4 +6,12 @@ describe('StringCalculator', () => {
         expect(calculator.add("")).toBe(0);
     });
 
+    test('should return 1 for an string "1"', ()=>{
+        expect(calculator.add('1')).toBe(1)
+    })
+
+    test('should return 3 for an string "1,2"', ()=>{
+        expect(calculator.add('1,2')).toBe(3)
+    })
+
 });
